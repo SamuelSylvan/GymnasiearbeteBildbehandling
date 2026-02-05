692c18bac569f5c1505441ae1a040d7b0aa0b315
Samuel Sylvan <samuel.sylvan@gmail.com>
2026-02-05 20:58:05 +0100

    Add project files.

---
+++ GymnasiearbeteWindowsForms/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace GymnasiearbeteWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTextPunkterInnanför = new System.Windows.Forms.Label();
+            this.lblIntPunkterInnanför = new System.Windows.Forms.Label();
+            this.lblTextAndelinnanför = new System.Windows.Forms.Label();
+            this.lblDecimalAndelInnanför = new System.Windows.Forms.Label();
+            this.lblTextAntalPunkter = new System.Windows.Forms.Label();
+            this.lblIntAntalPunkter = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnBeräkna = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnVit = new System.Windows.Forms.Button();
+            this.btnSvart = new System.Windows.Forms.Button();
+            this.btnVäljFärg = new System.Windows.Forms.Button();
+            this.btnUndersöktFärg = new System.Windows.Forms.Button();
+            this.lblUndersöktFärg = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTextPunkterInnanför
+            // 
+            this.lblTextPunkterInnanför.AutoSize = true;
+            this.lblTextPunkterInnanför.Location = new System.Drawing.Point(48, 32);
+            this.lblTextPunkterInnanför.Name = "lblTextPunkterInnanför";
+            this.lblTextPunkterInnanför.Size = new System.Drawing.Size(170, 20);
+            this.lblTextPunkterInnanför.TabIndex = 1;
+            this.lblTextPunkterInnanför.Text = "Antal punkter innanför:";
+            this.lblTextPunkterInnanför.Click += new System.EventHandler(this.lblTextPunkterInnanför_Click);
+            // 
+            // lblIntPunkterInnanför
+            // 
+            this.lblIntPunkterInnanför.AutoSize = true;
+            this.lblIntPunkterInnanför.Location = new System.Drawing.Point(224, 32);
+            this.lblIntPunkterInnanför.Name = "lblIntPunkterInnanför";
+            this.lblIntPunkterInnanför.Size = new System.Drawing.Size(14, 20);
+            this.lblIntPunkterInnanför.TabIndex = 2;
+            this.lblIntPunkterInnanför.Text = "-";
+            // 
+            // lblTextAndelinnanför
+            // 
+            this.lblTextAndelinnanför.AutoSize = true;
+            this.lblTextAndelinnanför.Location = new System.Drawing.Point(6, 75);
+            this.lblTextAndelinnanför.Name = "lblTextAndelinnanför";
+            this.lblTextAndelinnanför.Size = new System.Drawing.Size(212, 20);
+            this.lblTextAndelinnanför.TabIndex = 3;
+            this.lblTextAndelinnanför.Text = "Andel av punkterna innanför:";
+            // 
+            // lblDecimalAndelInnanför
+            // 
+            this.lblDecimalAndelInnanför.AutoSize = true;
+            this.lblDecimalAndelInnanför.Location = new System.Drawing.Point(224, 75);
+            this.lblDecimalAndelInnanför.Name = "lblDecimalAndelInnanför";
+            this.lblDecimalAndelInnanför.Size = new System.Drawing.Size(14, 20);
+            this.lblDecimalAndelInnanför.TabIndex = 4;
+            this.lblDecimalAndelInnanför.Text = "-";
+            // 
+            // lblTextAntalPunkter
+            // 
+            this.lblTextAntalPunkter.AutoSize = true;
+            this.lblTextAntalPunkter.Location = new System.Drawing.Point(112, 52);
+            this.lblTextAntalPunkter.Name = "lblTextAntalPunkter";
+            this.lblTextAntalPunkter.Size = new System.Drawing.Size(105, 20);
+            this.lblTextAntalPunkter.TabIndex = 5;
+            this.lblTextAntalPunkter.Text = "AntalPunkter:";
+            // 
+            // lblIntAntalPunkter
+            // 
+            this.lblIntAntalPunkter.AutoSize = true;
+            this.lblIntAntalPunkter.Location = new System.Drawing.Point(224, 52);
+            this.lblIntAntalPunkter.Name = "lblIntAntalPunkter";
+            this.lblIntAntalPunkter.Size = new System.Drawing.Size(14, 20);
+            this.lblIntAntalPunkter.TabIndex = 6;
+            this.lblIntAntalPunkter.Text = "-";
+            this.lblIntAntalPunkter.Click += new System.EventHandler(this.lblIntAntalPunkter_Click);
+            // 
+            // btnBeräkna
+            // 
+            this.btnBeräkna.Location = new System.Drawing.Point(135, 345);
+            this.btnBeräkna.Name = "btnBeräkna";
+            this.btnBeräkna.Size = new System.Drawing.Size(87, 32);
+            this.btnBeräkna.TabIndex = 7;
+            this.btnBeräkna.Text = "Beräkna";
+            this.btnBeräkna.UseVisualStyleBackColor = true;
+            this.btnBeräkna.Click += new System.EventHandler(this.btnBeräkna_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTextPunkterInnanför);
+            this.groupBox1.Controls.Add(this.lblTextAntalPunkter);
+            this.groupBox1.Controls.Add(this.lblDecimalAndelInnanför);
+            this.groupBox1.Controls.Add(this.lblIntAntalPunkter);
+            this.groupBox1.Controls.Add(this.lblTextAndelinnanför);
+            this.groupBox1.Controls.Add(this.lblIntPunkterInnanför);
+            this.groupBox1.Location = new System.Drawing.Point(135, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 146);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnVit);
+            this.groupBox2.Controls.Add(this.btnSvart);
+            this.groupBox2.Controls.Add(this.btnVäljFärg);
+            this.groupBox2.Controls.Add(this.btnUndersöktFärg);
+            this.groupBox2.Controls.Add(this.lblUndersöktFärg);
+            this.groupBox2.Location = new System.Drawing.Point(135, 173);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(298, 152);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // btnVit
+            // 
+            this.btnVit.Location = new System.Drawing.Point(94, 101);
+            this.btnVit.Name = "btnVit";
+            this.btnVit.Size = new System.Drawing.Size(82, 37);
+            this.btnVit.TabIndex = 13;
+            this.btnVit.Text = "Vit";
+            this.btnVit.UseVisualStyleBackColor = true;
+            this.btnVit.Click += new System.EventHandler(this.btnVit_Click);
+            // 
+            // btnSvart
+            // 
+            this.btnSvart.Location = new System.Drawing.Point(94, 60);
+            this.btnSvart.Name = "btnSvart";
+            this.btnSvart.Size = new System.Drawing.Size(82, 35);
+            this.btnSvart.TabIndex = 12;
+            this.btnSvart.Text = "Svart";
+            this.btnSvart.UseVisualStyleBackColor = true;
+            this.btnSvart.Click += new System.EventHandler(this.btnSvart_Click);
+            // 
+            // btnVäljFärg
+            // 
+            this.btnVäljFärg.Location = new System.Drawing.Point(94, 23);
+            this.btnVäljFärg.Name = "btnVäljFärg";
+            this.btnVäljFärg.Size = new System.Drawing.Size(82, 35);
+            this.btnVäljFärg.TabIndex = 11;
+            this.btnVäljFärg.Text = "Välj färg";
+            this.btnVäljFärg.UseVisualStyleBackColor = true;
+            this.btnVäljFärg.Click += new System.EventHandler(this.btnVäljFärg_Click);
+            // 
+            // btnUndersöktFärg
+            // 
+            this.btnUndersöktFärg.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnUndersöktFärg.Enabled = false;
+            this.btnUndersöktFärg.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnUndersöktFärg.Location = new System.Drawing.Point(183, 29);
+            this.btnUndersöktFärg.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUndersöktFärg.Name = "btnUndersöktFärg";
+            this.btnUndersöktFärg.Size = new System.Drawing.Size(86, 97);
+            this.btnUndersöktFärg.TabIndex = 10;
+            this.btnUndersöktFärg.UseVisualStyleBackColor = false;
+            // 
+            // lblUndersöktFärg
+            // 
+            this.lblUndersöktFärg.AutoSize = true;
+            this.lblUndersöktFärg.Location = new System.Drawing.Point(100, 49);
+            this.lblUndersöktFärg.Name = "lblUndersöktFärg";
+            this.lblUndersöktFärg.Size = new System.Drawing.Size(0, 20);
+            this.lblUndersöktFärg.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1924, 636);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBeräkna);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTextPunkterInnanför;
+        private System.Windows.Forms.Label lblIntPunkterInnanför;
+        private System.Windows.Forms.Label lblTextAndelinnanför;
+        private System.Windows.Forms.Label lblDecimalAndelInnanför;
+        private System.Windows.Forms.Label lblTextAntalPunkter;
+        private System.Windows.Forms.Label lblIntAntalPunkter;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnBeräkna;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblUndersöktFärg;
+        private System.Windows.Forms.Button btnUndersöktFärg;
+        private System.Windows.Forms.Button btnVäljFärg;
+        private System.Windows.Forms.Button btnVit;
+        private System.Windows.Forms.Button btnSvart;
+    }
+}
+
---
+++ GymnasiearbeteWindowsForms/Form1.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+
+
+namespace GymnasiearbeteWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        private readonly int offset = 40;
+        int decimalersNorganhet = 2; 
+        int nIterationer=1000;
+        private Color undersöktFärg;
+        private string filnamn = "ifylldElipse.png";
+        private Bitmap bmp;
+        private PictureBox bilden = new PictureBox();
+        private bool eyeDroperAktiverad = false;
+        static ThreadLocal<Random> ThreadLocalRandom = new ThreadLocal<Random>(() => new Random(Guid.NewGuid().GetHashCode()));
+        public Form1()
+        {
+            bmp = new Bitmap(filnamn);
+            bilden.Image = Image.FromFile(filnamn);
+            
+            bilden.SizeMode = PictureBoxSizeMode.AutoSize; // Automatisk storlek
+            bilden.Location = new Point(0, 0);
+            this.Controls.Add(bilden);
+            bilden.Visible = true;
+            InitializeComponent();
+            btnBeräkna.BringToFront();
+            groupBox1.BringToFront(); 
+            groupBox2.BringToFront();
+
+
+        }
+        
+        static bool UndersäkaOmPunktInnanför(int x_värde, int y_värde, Bitmap bmp)
+        {
+            int nKorsningar = 0;
+            bool påLinje = false;
+            for (int x = x_värde; x < bmp.Width; x++)
+            {
+                Color pixel = bmp.GetPixel(x, y_värde);
+                bool svart = pixel.R < 128;
+                if (svart && !påLinje)
+                {
+                    nKorsningar++;
+                    påLinje = true;
+                }
+                else if (!svart)
+                {
+                    påLinje = false;
+                }
+            }
+            if (nKorsningar % 2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        static bool UndersäkaOmPixelHarFärg(int x_värde, int y_värde, Bitmap bmp, Color färg)
+        {
+            if (bmp.GetPixel(x_värde, y_värde) == färg)
+            {
+                return false;
+            }
+            return true;
+
+        }
+        static void RitaPunkt(int x_värde, int y_värde, Graphics g, Brush brush)
+        {
+            int px = x_värde;
+            int py = y_värde;
+
+            Rectangle r = new Rectangle(px - 4, py - 4, 8, 8);
+            g.FillEllipse(brush, r);
+            g.DrawEllipse(Pens.Black, r);
+        }
+        static void BeräknaGrafiskt()
+        {
+
+        }
+
+        private void Bilden_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Bilden_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnUndersöktFärg.BackColor = bmp.GetPixel(e.X, e.Y);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            
+        }
+
+
+        private void lblIntAntalPunkter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTextPunkterInnanför_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBeräkna_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("data.txt", "");
+            Dictionary<double, int> värden = new Dictionary<double, int>();
+            btnBeräkna.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            bilden.Image = Image.FromFile(filnamn);
+            int pixelHöjd = bmp.Height;
+            int pixelBred = bmp.Width;
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bildData = bmp.LockBits(rect, ImageLockMode.ReadOnly, bmp.PixelFormat);
+            int stride = bildData.Stride;
+            IntPtr scan0 = bildData.Scan0;
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\SamuelSyl\source\repos\GymnasiearbeteWindowsForms\GymnasiearbeteWindowsForms\data.txt", false)) //Tömmer data
+            {
+                writer.Write("");
+            }
+            try
+            {
+                for (int nPunkter = 100; nPunkter <= 100; nPunkter = nPunkter * 10)
+                {
+                    for (int ii = 0; ii < nIterationer; ii++)
+                    {
+                        Stopwatch sw = new Stopwatch();
+                        
+                        int nPunkterInanför = 0;
+                        lblDecimalAndelInnanför.Visible = true;
+                        sw.Start();
+
+                        Random random = new Random();
+
+                        Parallel.For(
+                            0,
+                            nPunkter,
+                            () => 0,
+                            (i, state, localCount) =>
+                            {
+
+                                var rnd = ThreadLocalRandom.Value;
+                                int x_värde = rnd.Next(pixelBred);
+                                int y_värde = rnd.Next(pixelHöjd);
+                                unsafe
+                                {
+                                    byte* row = (byte*)scan0 + y_värde * stride;
+                                    byte* pixel = row + x_värde * 4; // 32bpp ARGB
+
+                                byte b = pixel[0];
+                                    byte g = pixel[1];
+                                    byte r = pixel[2];
+                                    byte a = pixel[3];
+                                    Color färgValdPixel = Color.FromArgb(a, r, g, b);
+                                    if (färgValdPixel == undersöktFärg)
+                                    {
+                                        localCount++;
+                                    }
+                                }
+
+                                return localCount;
+                            },
+                            localCount =>
+                            {
+                                Interlocked.Add(ref nPunkterInanför, localCount);
+                            });
+                        double andelInanför=Math.Round((double)nPunkterInanför / nPunkter, decimalersNorganhet);
+                        try
+                        {
+                            värden[andelInanför]++;
+                        }
+                        catch (Exception)
+                        {
+                            värden.Add(andelInanför, 1);
+                        }
+                        sw.Stop();
+                        lblDecimalAndelInnanför.Text = ((double)nPunkterInanför / nPunkter).ToString("F2");
+                        lblIntPunkterInnanför.Text = nPunkterInanför.ToString();
+                        lblIntAntalPunkter.Text = nPunkter.ToString();
+                    }
+                    double summaAndelarInanför = 0;
+                    double summaKvadreradeDiferanser = 0;
+                    double summaArea = 0;
+                    using (StreamWriter writer = new StreamWriter(@"C:\Users\SamuelSyl\source\repos\GymnasiearbeteWindowsForms\GymnasiearbeteWindowsForms\data.txt", false))
+                    {
+                        writer.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+                        foreach (KeyValuePair<double, int> item in värden)
+                        {
+                            summaAndelarInanför += item.Key* item.Value;
+                            double höjd = ((double)item.Value / nIterationer) / Math.Pow(10, decimalersNorganhet * -1);
+                            summaArea += höjd * Math.Pow(10, decimalersNorganhet * -1);
+                            writer.WriteLine($"{Math.Round(item.Key,decimalersNorganhet).ToString().Replace(",", ".")},{(höjd).ToString().Replace(",", ".")}");
+                        }
+                        double summaAndelAvIterationer = 0;
+                        double medelvärdeAndelInnanför = summaAndelarInanför / nIterationer; 
+                        foreach (KeyValuePair<double, int> item in värden)
+                        {
+                            summaAndelAvIterationer += (double)item.Value / nIterationer;
+                            summaKvadreradeDiferanser += Math.Pow(((double)item.Key * item.Value)/nPunkter - medelvärdeAndelInnanför, 2);
+                        }
+                        double standardavikelse = Math.Sqrt(summaKvadreradeDiferanser / (nIterationer - 1));
+                        writer.WriteLine($"STANDARDAVIKELSE {standardavikelse} ");
+                        writer.WriteLine($"MEDELVÄRDE {medelvärdeAndelInnanför}");
+                        writer.WriteLine($"AREA {summaArea}");
+                        writer.WriteLine($"SUMMA ANDELAR Innanför {summaAndelarInanför}");
+                        writer.WriteLine($"SUMMA ANDELAR {summaAndelAvIterationer}");
+                    }
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bildData);
+            }
+            this.Cursor = Cursors.Default;
+            btnBeräkna.Enabled = true;
+            /*
+            Dictionary<int, int> värden = new Dictionary<int, int>();
+            btnBeräkna.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            bilden.Image = Image.FromFile(filnamn);
+            for (int ii = 0; ii < 100; ii++)
+            {
+                using (Graphics g = Graphics.FromImage(bilden.Image))
+                {
+                    Stopwatch sw = new Stopwatch();
+                    int nPunkter = 1000;
+                    int nPunkterInanför = 0;
+                    lblDecimalAndelInnanför.Visible = true;
+                    sw.Start();
+
+                    Random random = new Random();
+
+                    for (int i = 0; i < nPunkter; i++)
+                    {
+                        int x_värde = random.Next(bmp.Width);
+                        int y_värde = random.Next(bmp.Height);
+                        if (UndersäkaOmPixelHarFärg(x_värde, y_värde, bmp, undersöktFärg))
+                        {
+                            RitaPunkt(x_värde, y_värde, g, Brushes.Red);
+                            nPunkterInanför++;
+                        }
+                        else
+                        {
+                            RitaPunkt(x_värde, y_värde, g, Brushes.Blue);
+                        }
+                        bilden.Refresh();
+                    }
+                    try
+                    {
+                        värden[nPunkterInanför]++;
+                    }
+                    catch (Exception)
+                    {
+                        värden.Add(nPunkterInanför, 1);
+                    }
+                    sw.Stop();
+                    lblDecimalAndelInnanför.Text = ((double)nPunkterInanför / nPunkter).ToString("F2");
+                    lblIntPunkterInnanför.Text = nPunkterInanför.ToString();
+                    lblIntAntalPunkter.Text = nPunkter.ToString();
+                }
+                this.Cursor = Cursors.Default;
+            }
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\SamuelSyl\source\repos\GymnasiearbeteWindowsForms\GymnasiearbeteWindowsForms\data.txt", true))
+            {
+
+
+                foreach (KeyValuePair<int, int> item in värden)
+                {
+                    writer.WriteLine($"{item.Key},{item.Value}");
+                }
+
+            }
+            btnBeräkna.Enabled = true;
+            */
+            //_______________________________________________________________
+            //base.OnPaint(e);
+            //Graphics g = e.Graphics;
+
+            // --- rita axlar ---
+            //Pen axisPen = new Pen(Color.Black, 2);
+            //g.DrawLine(axisPen, offset, image.Height + offset, image.Width + offset, image.Height + offset);
+            //g.DrawLine(axisPen, offset, offset, offset, image.Height + offset);
+
+        }
+
+        private void btnVäljFärg_Click(object sender, EventArgs e)
+        {
+            if (eyeDroperAktiverad)
+            {
+                this.Cursor = Cursors.Default;
+                btnBeräkna.Enabled = true;
+                btnSvart.Enabled = true;
+                btnVit.Enabled = true;
+                bilden.MouseEnter -= Bilden_MouseEnter;
+                bilden.MouseMove -= Bilden_MouseMove;
+                bilden.MouseLeave -= Bilden_MouseLeave;
+                bilden.MouseClick -= Bilden_MouseClick;
+                btnVäljFärg.Text = "Välj färg";
+            }
+            else
+            {
+                this.Cursor = Cursors.Cross;
+                bilden.MouseEnter += Bilden_MouseEnter;
+                bilden.MouseMove += Bilden_MouseMove;
+                bilden.MouseLeave += Bilden_MouseLeave;
+                bilden.MouseClick += Bilden_MouseClick;
+                btnVäljFärg.Text = "Avbryt";
+                btnBeräkna.Enabled = false;
+                btnSvart.Enabled = false;
+                btnVit.Enabled = false;
+            }
+            eyeDroperAktiverad = !eyeDroperAktiverad;
+        }
+
+        private void BtnVäljFärg_MouseClickAvbryt(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Bilden_MouseClick(object sender, MouseEventArgs e)
+        {
+            undersöktFärg = bmp.GetPixel(e.X, e.Y);
+            btnUndersöktFärg.BackColor = undersöktFärg;
+            this.Cursor = Cursors.Default;
+            bilden.MouseEnter -= Bilden_MouseEnter;
+            bilden.MouseMove -= Bilden_MouseMove;
+            bilden.MouseLeave -= Bilden_MouseLeave;
+            bilden.MouseClick -= Bilden_MouseClick;
+            btnVäljFärg.Text = "Välj färg";
+            btnBeräkna.Enabled = true;
+            btnBeräkna.Enabled = true;
+            btnSvart.Enabled = true;
+            btnVit.Enabled = true;
+            eyeDroperAktiverad = false;
+        }
+
+        private void Bilden_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Cross;
+        }
+
+        private void Bilden_MouseEnter(object sender, EventArgs e)
+        {
+            var eyeDropperCursor = new Cursor("eyedropper.cur");
+            this.Cursor = eyeDropperCursor;
+        }
+
+        private void btnSvart_Click(object sender, EventArgs e)
+        {
+            btnUndersöktFärg.BackColor = Color.Black;
+            undersöktFärg = Color.Black;
+        }
+
+        private void btnVit_Click(object sender, EventArgs e)
+        {
+            //btnUndersöktFärg.BackColor = Color.White;
+            //undersöktFärg = Color.White;
+            
+            /*
+             * 
+             * using (StreamWriter writer = new StreamWriter("data.txt"))
+            {
+                
+            }Stopwatch sw = new Stopwatch();
+            int nPunkter = 10;
+            int nPunkterInanför = 0;
+            lblDecimalAndelInnanför.Visible = true;
+            sw.Start();
+            Dictionary<int, int> värden = new Dictionary<int, int>();
+            Random random = new Random();
+            for (int i = 0; i < 1; i++)
+            {
+                for (int ii = 0; ii < nPunkter; i++)
+                {
+                    int x_värde = random.Next(bmp.Width);
+                    int y_värde = random.Next(bmp.Height);
+                    if (UndersäkaOmPixelHarFärg(x_värde, y_värde, bmp, undersöktFärg))
+                    {
+                        nPunkterInanför++;
+                    }
+                    
+                }
+            }
+            
+            
+            
+            sw.Stop();
+            */
+        }
+    }
+}
+
